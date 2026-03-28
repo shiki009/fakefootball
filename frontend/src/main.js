2183aab87111de6967dc0d46b5a317c171e224e9
shiki009 <vladislavshik10@gmail.com>
2026-03-28 23:52:00 +0200

    feat: add Vercel Analytics

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { inject } from '@vercel/analytics'
 import router from './router.js'
 import App from './app.vue'
 import './style.css'
+
+inject()
 
 const app = createApp(App)
 app.use(createPinia())
